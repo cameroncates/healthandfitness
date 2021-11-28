770eb35ade8c6b538efe3484988e03171c7cd85c
Manish2055 <92129123+Manish2055@users.noreply.github.com>
2021-11-28 16:09:39 +1100

    Update universalFitnessSurveys.js

--- assets/surveys/universalFitnessSurveys.js
+++ assets/surveys/universalFitnessSurveys.js
@@ -2,8 +2,8 @@ export const survey = {
     list: [
       
         {
-            title:
-                <p><b>20 metre run</b> - number of 20m laps in 5 minutes. This is the classic test of aerobic fitness, superseding the 'beep test'.</p> 
+            title:`
+               <p><b>20 metre run</b> - number of 20m laps in 5 minutes. This is the classic test of aerobic fitness, superseding the 'beep test'.</p> 
                 One foot must go beyond the line at the end of each lap.
                 <br><br>
                 It may take you several attempts to work out the best speed to start off with. You can walk, shuffle, jog or run. If you're running and you run out of puff you can slow down to a walk.
